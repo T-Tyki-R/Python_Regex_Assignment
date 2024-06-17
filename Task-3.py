4c2c900243ef85ee88da61bd03a1f5a5d170f13c
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-06-17 12:59:56 -0400

    Task 3 and 4 Completed

--- Task-3.py
+++ Task-3.py
@@ -4,3 +4,7 @@ import re
 text = "Name: John Doe; Age: 30; Occupation: Engineer; Location: New York"
 # Extract the Occupation from the text
 # Your code here
+str_split_res = re.split(";", text)
+for i in str_split_res:
+    ind_res = re.split(":", i)
+    print(f"{ind_res[-1]}")
--- Task-4.py
+++ Task-4.py
@@ -7,3 +7,20 @@ descriptions = [
 ]
 # Tag each product based on keywords in the description
 # Your code here
+
+electronics = ""
+apparel = ""
+home_kitchen = ""
+
+for x in descriptions:
+    item_res = re.split(" ", x)
+    if 'screen' in item_res or 'screen'.capitalize() in item_res:
+        electronics += "Electonic : Smartphone"
+    if 'cotton' in item_res or 'cotton'.capitalize() in item_res:
+       apparel += "Apparel : T-Shirt"
+    if 'knife'.capitalize() in item_res or 'knife' in item_res:
+        home_kitchen += "Home and Kitchen : Knife Set"
+
+print(electronics)
+print(apparel)
+print(home_kitchen)
